fc0c2c9589b5eceafc796f15ddc147beb1d3e300
Paulo Henrique dos Santos <paulo.santos@poatek.com>
2018-08-25 18:16:25 -0300

    Add HGTest functionality

--- app/src/main/java/com/example/bottomnavigation/ui/AddHGTestActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/ui/AddHGTestActivity.kt
@@ -2,8 +2,12 @@ package com.example.bottomnavigation.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.bottomnavigation.DataManager.HGTest
 import com.example.bottomnavigation.DataManager.HGTestType
+import com.example.bottomnavigation.DataManager.TestDataManager
 import com.example.bottomnavigation.R
+import kotlinx.android.synthetic.main.activity_add_hgtest.*
 import kotlinx.android.synthetic.main.dialog_add.*
 
 class AddHGTestActivity : AppCompatActivity() {
@@ -11,18 +15,34 @@ class AddHGTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_hgtest)
+
+        confirmarAdd.setOnClickListener { view ->
+            confirm(view = view)
+        }
+
+        cancel2.setOnClickListener {
+            cancel()
+        }
     }
 
-    fun confirm() {
-        val testName = editText.text
-
+    fun confirm(view: View) {
+        val testName = nameText.text
 
         if (!testName.isBlank()) {
+            var type = HGTestType.DISTANCE
 
+            if (radioTempo.isChecked) {
+                type = HGTestType.TIME
+            } else if (radioRepeticoes.isChecked) {
+                type = HGTestType.COUNT
+            }
+
+            TestDataManager.sharedInstance.testList.add(HGTest(name = testName.toString(), type = type))
+            finish()
         }
     }
 
     fun cancel() {
-        this.finish()
+        finish()
     }
 }
